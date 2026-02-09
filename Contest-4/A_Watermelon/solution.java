1b5195702c8b939042976a091078b5a899db6c14
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-02-09 20:49:58 +0530

    Add solution 4A

---
+++ Contest-4/A_Watermelon/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int w=sc.nextInt();
+        if(w<4){
+            System.out.println("NO");
+        }
+        else if(w%2==0){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }    
+}
