34ac63a51e05fb92f25fe7a6f65065874a260394
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-02-09 20:38:02 +0530

    Add solution 443A

---
+++ Contest-443/A_Anton_and_Letters/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+        public static void main(String args[]){
+                Scanner sc=new Scanner(System.in);
+                String s=sc.nextLine();
+                HashSet<Character> set=new HashSet<>();
+                for(char c:s.toCharArray()){
+                        if(c>='a' && c<='z'){
+                                set.add(c);
+                        }
+                }
+                System.out.println(set.size());
+        }
+}
